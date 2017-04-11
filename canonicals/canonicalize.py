d450fb293a6e4ff8ab65d7d35099460394147fa2
Matt Carabine <matt.carabine@hotmail.co.uk>
2017-04-12 00:51:34 +0100

    Fix canonicalization script

--- canonicals/canonicalize.py
+++ canonicals/canonicalize.py
@@ -4,20 +4,23 @@ import os
 import sys
 
 def main():
-    output_dir = os.abspath(sys.argv[1])
+    output_dir = os.path.abspath(sys.argv[1])
     os.chdir(sys.path[0])
     with open('canonicals.csv', 'r') as csv_f:
         reader = csv.reader(csv_f)
         for row in reader:
             source_file, canonical_url = row
-            with open(os.path.join([
+            with open(os.path.join(
                     output_dir, source_file.replace('.dita', '.html')
-                    ])) as html_f: 
+                    ), 'r+') as html_f:
                 soup = bs4.BeautifulSoup(html_f.read(), 'html.parser')
-                link = soup.new_tag('link')
-                link.attrs['rel'] = 'canonical'
-                link.attrs['href'] = canonical_url
+                link = soup.new_tag('meta')
+                link.attrs['name'] = 'canonical_url'
+                link.attrs['content'] = canonical_url
                 soup.head.insert(0, link)
+                html_f.seek(0)
+                html_f.write(soup.prettify())
+
 
 if __name__ == '__main__':
     main()
